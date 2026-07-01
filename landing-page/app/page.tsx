f188df9d3a6de9303927f44038b9bead7366a510
MPIO1002 <minhphuc309@gmail.com>
2026-07-01 10:02:57 +0700

    feat: add premium HeroSection in Vietnamese with video controls

---
+++ landing-page/app/page.tsx
@@ -0,0 +1,11 @@
+
+import HeroSection from "@/components/HeroSection";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-black">
+      <HeroSection />
+    </main>
+  );
+}
+
